51401ed10e6991b5813253d1d7d6c206c31a6d43
ErasylAbenov <abenov.erasyl@gmail.com>
2020-06-23 07:42:32 +0600

    added put and delete

--- sarafan/src/main/java/letscode/sarafan/controller/MessageController.java
+++ sarafan/src/main/java/letscode/sarafan/controller/MessageController.java
@@ -1,10 +1,7 @@
 package letscode.sarafan.controller;
 
 import letscode.sarafan.exceptions.NotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,6 +11,8 @@ import java.util.Map;
 @RestController
 @RequestMapping("message")
 public class MessageController {
+    private int counter = 4;
+
     public List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
         add(new HashMap<String, String>() {{ put("id", "1"); put("text", "1 message");}});
         add(new HashMap<String, String>() {{ put("id", "2"); put("text", "2 message");}});
@@ -27,9 +26,38 @@ public class MessageController {
 
     @GetMapping("{id}")
     public Map<String, String> getOne(@PathVariable String id) {
+        return getMessage(id);
+    }
+
+    private Map<String, String> getMessage(@PathVariable String id) {
         return messages.stream()
                 .filter(message -> message.get("id").equals(id))
                 .findFirst()
                 .orElseThrow(NotFoundException::new);
+    }
+
+    @PostMapping
+    public Map<String, String> create(@RequestBody Map<String, String> message) {
+        message.put("id", String.valueOf(counter++));
+
+        messages.add(message);
+
+        return message;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update(@PathVariable String id, @RequestBody Map<String, String> message) {
+        Map<String, String> messageFromDb = getMessage(id);
+
+        messageFromDb.putAll(message);
+        messageFromDb.put("id", id);
+
+        return messageFromDb;
+    }
+
+    @DeleteMapping("id")
+    public void delete(@PathVariable String id) {
+        Map<String, String> message = getMessage(id);
+        messages.remove(message);
     }
 }
